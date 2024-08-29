5344556626684a47d29d062e0558914ade733bd3
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-08-29 18:39:37 +0300

    updated the readme

---
+++ cron.route.ts
@@ -0,0 +1,12 @@
+
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export async function GET(request: Request) {
+  try {
+    
+  } catch (error: any) {
+    throw new Error(`Failed to get all products: ${error.message}`);
+  }
+}
