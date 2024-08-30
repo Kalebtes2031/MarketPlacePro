26e286a9992b2bc4a086b8e7860fe5a393ba9f5f
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-08-30 11:14:51 +0300

    added the home page

---
+++ marketplacepro/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+const space_Grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "MarketPlacePro",
+  description: "Track products' price online and save money while shopping.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          <Navbar/>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
