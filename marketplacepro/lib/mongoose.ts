c65099c6461f5d42bf456e4e31d7df7a2fbf947d
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-09-06 13:18:33 +0300

    updated

--- marketplacepro/lib/mongoose.ts
+++ marketplacepro/lib/mongoose.ts
@@ -1,21 +1,27 @@
 import mongoose from 'mongoose';
 
-let isConnected = false;// Variable to track the connection status
+let isConnected = false;
 
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if(!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
+  if (!process.env.MONGODB_URI) {
+    console.log('MONGODB_URI is not defined');
+    return;
+  }
 
-  if(isConnected) return console.log('=> using existing database connection');
+  if (isConnected) {
+    console.log('=> using existing database connection');
+    return;
+  }
 
   try {
+    console.log('Attempting to connect to MongoDB...');
     await mongoose.connect(process.env.MONGODB_URI);
 
     isConnected = true;
-
     console.log('MongoDB Connected');
   } catch (error) {
-    console.log(error)
+    console.error('Error connecting to MongoDB:', error);
   }
-}
+};
